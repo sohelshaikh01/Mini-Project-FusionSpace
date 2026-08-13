7205fa3f1eb3f3d98bba09501ca6d99e22ba3fc3
Sohel Shaikh <sohel.techin@gmail.com>
2026-08-13 23:47:37 +0530

    Refactor: Port No Message Update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ connectDB()
     });
 
     app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port 8000`);
+        console.log(`Server is running on port ${process.env.PORT}`);
     });
 })
 .catch((error) => {
